dcc87f046cbc842eef881695dd1be456deb57731
krishkashiwala <krishkashiwala@gmail.com>
2021-12-04 19:46:43 +0530

    repo container almost ready

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import RepoContainer from "./RepoContainer";
 const Profile = () => {
 	const location: any = useLocation()
+	const [datas, setDatas] = React.useState() as any
 	console.log(location)
-	let urls = `https://api.github.com/users?id=${location?.state?.user}`
-	let temp = async () => {
-		let data = await fetch(urls).then(res => res.json())
+	const API_CLIENT_ID = '5f749d7ba5fd5061d9dc'
+	const API_CLIENT_SECRET = '401864132901e04abe89a508b2f75fec78a26690'
+	const API_URL = 'https://api.github.com/users'
+	const temp = async () => {
+		let data = await fetch(`${API_URL}/${location?.state?.user}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`).then(res => res.json()).catch(e => console.log(e))
+		setDatas(data)
 		console.log(data)
 	}
-	temp()
+
+	React.useEffect(() => {
+		temp()
+
+		// eslint-disable-next-line
+	}, [])
+	console.log(datas)
 	return (
-		<div>
-			<h1>Hello world</h1>
+		<div className="profile-container">
+			<RepoContainer user={location?.state?.user} API_URL={API_URL} API_CLIENT_ID={API_CLIENT_ID} API_CLIENT_SECRET={API_CLIENT_SECRET} />
 		</div>
 	)
 }
---
+++ src/components/RepoContainer.tsx
@@ -0,0 +1,56 @@
+import React from "react"
+import Repo from "./Repo"
+
+const RepoContainer: React.FC<{ user: string | undefined, API_URL: string, API_CLIENT_ID: string, API_CLIENT_SECRET: string }> = ({ API_URL, API_CLIENT_ID, API_CLIENT_SECRET, user }) => {
+	const [customDrop, setCustomDrop] = React.useState(false)
+	const [data, setData] = React.useState() as any
+	const temp = async () => {
+		let data = await fetch(`${API_URL}/${user}/repos?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`).then(res => res.json()).catch(e => console.log(e))
+		setData(data)
+		console.log(data)
+	}
+
+	React.useEffect(() => {
+		temp()
+		// eslint-disable-next-line
+	}, [])
+	console.log(data)
+	return (
+
+		<div className="repo-container">
+			<div className="sub-repo-container">
+				<h2>Top Repos</h2>
+				<span>by</span>
+				<div className="dropdown">
+					<button className="button_dropdown" onClick={() => setCustomDrop(!customDrop)}>
+						<label>Stars</label>
+						<svg aria-hidden="true" height="16" role="img" viewBox="0 0 12 16" width="12" ><path fill-rule="evenodd" d="M0 5l6 6 6-6H0z"></path></svg>
+					</button>
+					{
+						customDrop && <ul>
+
+							<li>Stars</li>
+							<li>Repos</li>
+							<li>Forks</li>
+						</ul>
+					}
+
+				</div>
+			</div>
+			<div className="repo-content-container">
+				{
+					data?.slice(0, 8).map((item: any) => (
+						<Repo
+							name={item?.name}
+							language={item?.language}
+							url={item?.html_url}
+							fork_count={item?.forks_count}
+							watchers_count={item?.watchers_count}
+						/>
+					))
+				}
+			</div>
+		</div>
+	)
+}
+export default RepoContainer;
